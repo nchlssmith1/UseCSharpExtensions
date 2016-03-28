885585e245ed1fa7c92e3312a470488db319116c
Nicholas Smith <nchlssmith1@gmail.com>
2016-03-27 19:31:43 -0500

    create project

---
+++ UsefulCSharpExtensions/UsefulCSharpExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UsefulCSharpExtensions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
